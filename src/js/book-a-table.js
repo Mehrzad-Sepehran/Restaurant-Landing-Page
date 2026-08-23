7347071d35b920b163061d0170d533d9e858fb25
MehrzaD <sepehranmehrzad@gmail.com>
2026-08-23 19:29:37 +0330

    feat: add toast

--- src/js/book-a-table.js
+++ src/js/book-a-table.js
@@ -6,6 +6,9 @@ const dateInput = document.querySelector("#date-input");
 const guestInput = document.querySelector("#guest-input");
 const specialRequestsInput = document.querySelector("#special-requests-input");
 const requiredInputs = document.querySelectorAll(".required-input");
+const toastContainer = document.querySelector("#toast-container");
+const toastText = document.querySelector("#toast-text");
+const toastCloseBtn = document.querySelector("#toast-close-btn");
 
 const checkInputs = () => {
   let unfilledInputsCount = 0;
@@ -24,11 +27,27 @@ const checkInputs = () => {
   } else return true;
 };
 
+const showToast = (message) => {
+  toastText.innerHTML = message
+  toastContainer.classList.add("top-20");
+  toastContainer.classList.remove("opacity-0");
+}
+
+const hideToast = () => {
+  toastContainer.classList.remove("top-20");
+  toastContainer.classList.add("opacity-0");
+}
+
 reservstionBtn.addEventListener("click", (event) => {
   event.preventDefault();
   const areIputsFlled = checkInputs();
+
 
   if (areIputsFlled) {
   } else {
   }
 });
+
+toastCloseBtn.addEventListener("click", () => {
+  hideToast()
+})
